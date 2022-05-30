5c63daa168b01788c460bc5f42d18dd9ab621d0c
Seaver Milnor <milnor@users.noreply.github.com>
2022-05-30 22:01:41 +0200

    readelf lite

--- include/bin_formats.h
+++ include/bin_formats.h
@@ -1,16 +1,35 @@
 #ifndef BIN_FORMATS_H
 #define BIN_FORMATS_H
 
-#define DOS_MAGIC       0x4D5A      // "MZ"
-#define ELF_MAGIC       0x7F454C46  // x7F ELF
-#define MACHO_MAGIC     0xFEEDFACE
-#define PE_MAGIC        0x50450000  // "PE\0\0"
+#define DOS_MAGIC       "\x4D\x5A"          // "MZ"
+#define ELF_MAGIC       "\x7F\x45\x4C\x46"  // x7F ELF
+#define MACHO_MAGIC     "\xFE\xED\xFA\xCE"
+#define PE_MAGIC        "\x50\x45\x00\x00"  // "PE\0\0"
 
 typedef enum
 {
+    UNKNOWN = -1,
     ELF,
     MACHO,
     PE
 } exec_fmt_t;
+
+typedef enum
+{
+    OTHER = -1,
+    ARM,
+    X86
+} cpu_arch_t;
+
+/* Storing the string pointers is inefficient, but ergonomic */
+typedef struct
+{
+    exec_fmt_t format;
+    cpu_arch_t cpu;
+    char arch_bits;
+    char * format_str;
+    char * cpu_str;
+    char endian;
+} obj_fmt_t;
 
 #endif
--- src/lale.c
+++ src/lale.c
@@ -1,8 +1,10 @@
 #include "../include/bin_formats.h"
 
+#include <elf.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -13,12 +15,68 @@
 #endif
 
 // move these to header
-int detect_format(uint8_t * magic, size_t len);
+exec_fmt_t detect_format(uint8_t * magic, size_t len);
+void parse_elf_ident(obj_fmt_t * object, FILE * target);
 
-int detect_format(uint8_t * magic, size_t len)
+void parse_elf_ident(obj_fmt_t * object, FILE * target)
 {
-    // TODO: implement me
-    return ELF;
+    object->format = ELF;
+    object->format_str = "ELF";
+
+    // cf. man 5 elf
+    unsigned char ident[EI_NIDENT] = { 0 };
+    rewind(target);
+    if (EI_NIDENT != fread(ident, sizeof(char), EI_NIDENT, target))
+    {
+        perror("fread");
+        return;
+    } 
+
+    switch (ident[EI_CLASS])
+    {
+        case ELFCLASS32:
+            object->arch_bits = 32;
+            break;
+        case ELFCLASS64:
+            object->arch_bits = 64;
+            break;
+        default:
+            // something is wrong
+            object->arch_bits = -1;
+    }
+
+    switch(ident[EI_DATA])
+    {
+        case ELFDATA2LSB:
+            object->endian = 'L';
+            break;
+        case ELFDATA2MSB:
+            object->endian = 'B';
+            break;
+        default:
+            // something is wrong
+            object->endian = '?';
+    }
+}
+
+exec_fmt_t detect_format(uint8_t * magic, size_t len)
+{
+    // TODO: add add'l formats
+    obj_fmt_t object =
+    {
+        .format = UNKNOWN,
+        .cpu = OTHER,
+        .arch_bits = 0,
+        .format_str = "UNKNOWN",
+        .cpu_str = "OTHER"
+    };
+
+    if (0 == strncmp(magic, ELF_MAGIC, sizeof(uint32_t)))
+    {
+        return ELF;
+    }
+
+    return UNKNOWN;
 }
 
 int main(int argc, char ** argv)
@@ -47,11 +105,32 @@ int main(int argc, char ** argv)
     if (sizeof(uint32_t) != fread(&magic, sizeof(uint8_t), sizeof(uint32_t), target))
     {
        perror("fread");
+       return EXIT_FAILURE;
     }
-    else
+
+    exec_fmt_t format = detect_format((uint8_t *)&magic, sizeof(uint32_t));
+    obj_fmt_t object = { 0 };
+    switch (format)
     {
-        printf("[!] File format = %d\n", detect_format((uint8_t *)&magic, sizeof(uint32_t)));
+        case ELF:
+            parse_elf_ident(&object, target);
+            break;
+        case UNKNOWN:
+        case MACHO:
+        case PE:
+        default:
+            printf("[!] Haven't implemented support for non-ELFs\n");
+            break;
     }
+
+    printf("[!] %s is a %c.Endian %u-bit %s (%d) object for %s (%d)\n",
+         argv[1], 
+         object.endian,
+         object.arch_bits, 
+         object.format_str,
+         object.format,
+         object.cpu_str,
+         object.cpu);
 
 	fclose(target);
 
